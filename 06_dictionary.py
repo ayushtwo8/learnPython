04b1d57c499a3ddbf9d14d393e5789beef58c703
Ayush Tiwari <133810590+ayushtwo8@users.noreply.github.com>
2024-06-27 17:08:15 +0530

    dict done

--- 06_dictionary.py
+++ 06_dictionary.py
@@ -2,7 +2,7 @@
 # ordered, changeable and do not allow duplicate.
 thisdict = {
     "brand":"ford",
-    "model":"model",
+    "model":"mustang",
     "year" : 1964
 }
 print(thisdict)
@@ -27,3 +27,40 @@ print(thisdict.items())
 # update() method
 thisdict.update({"year":1920})
 print(thisdict)
+
+#pop() method
+student = {
+    "name":"Rohan",
+    "age":25,
+    "college":"IIT Delhi",
+    "friend":"Sohan"
+}
+
+print(student.pop("friend"))
+print(student)
+
+#popitem() - removes the last inserted item
+print(student.popitem())
+print(student)
+
+#del keyword
+del student["age"]
+print(student)
+
+del student #student dictionary is deleted here
+
+#clear() method can be used to empty dictionary
+
+#copy a dictionary
+car1 = {
+    "brand" : "tesla",
+    "model" : "tesla x",
+    "year" : 2020
+}
+
+car2 = car1.copy()
+print(car2)
+
+# using dict function
+car3 = dict(car1)
+print(car3)
